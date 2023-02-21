13e29ed97e5adb61937d159edfa8075b4bf3533c
Sespi <sespickard24@gmail>
2023-02-21 18:30:54 -0500

    tiny fixes

--- tensor/app.py
+++ tensor/app.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 20)
+x = np.linspace(0, 20, 1000)
 plt.plot(x, np.sin(x))
 
-plt.title("The Seth-Coaster")
+plt.title("The Seth Coaster")
 plt.xlabel("Length of roller coaster")
 plt.ylabel("Height of hills")
 
