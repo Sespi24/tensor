1b03c229b3f88e85ee46a8fd425921e91563542d
Sespi <sespickard24@gmail>
2023-02-21 16:42:22 -0500

    Added labels

--- tensor/app.py
+++ tensor/app.py
@@ -3,5 +3,11 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 20)
 plt.plot(x, np.sin(x))
+
+plt.title("The Seth-Coaster")
+plt.xlabel("Length of roller coaster")
+plt.ylabel("Height of hills")
+
 plt.show()
+
 print ("hello world")
